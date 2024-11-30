37f125660eec323f00c843d25283d495f13ca235
Tan233135 <artanvir11@gmail.com>
2024-11-30 22:00:23 +0600

    second commit

---
+++ Codes1-10.py
@@ -0,0 +1,9 @@
+#1.Hello world
+print("Hello world")
+#2.Sum of two numbers
+def sum_two_num(a,b):
+    return a+b
+
+a=int(input())
+b=int(input())
+print(f"Sum of {a} and {b} is {sum_two_num(a,b)}")
