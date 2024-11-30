37b46f59b2c5ac25b22deea57f44471f3e3c8810
Tan233135 <artanvir11@gmail.com>
2024-11-30 22:01:53 +0600

    third commit

--- Codes1-10.py
+++ Codes1-10.py
@@ -7,3 +7,26 @@ def sum_two_num(a,b):
 a=int(input())
 b=int(input())
 print(f"Sum of {a} and {b} is {sum_two_num(a,b)}")
+#3. Sqare root of a number
+import math
+def sqrt_num(n):
+    return math.sqrt(num)
+x=int(input())
+print(f"The sqare root of {x} is {sqrt_num(x)}")
+#4.Triangle area
+def tri_area(a,b,c):
+    s=(a+b+c)/2
+    return math.sqrt(s*(s-a)*(s-b)*(s-c))
+a=int(input())
+b=int(input())
+c=int(input())
+print(f"The area of the triangle is {tri_area(a,b,c)}")
+#5.Even or odd
+def even_odd(x):
+    if x%2==0:
+        return "Even"
+    else:
+        return "ODD"
+x=int(input())
+print(f"The number is {even_odd(x)}")
+
